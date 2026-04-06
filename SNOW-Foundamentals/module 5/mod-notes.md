b905431a3b8df2d554e335e0b8d24f0c5728eda4
LevonHill <Levonh18@aol.com>
2026-04-05 23:28:00 -0400

    cont at work

--- SNOW-Foundamentals/module 5/mod-notes.md
+++ SNOW-Foundamentals/module 5/mod-notes.md
@@ -46,3 +46,23 @@ workflows for knowledge
 ![alt text](image-40.png)
 ![alt text](image-41.png)
 ![xl](image-42.png)
+
+![alt text](image-43.png)
+
+![alt text](image-44.png)
+
+![alt text](image-45.png)
+
+![alt text](image-46.png)
+
+![alt text](image-47.png)
+
+![alt text](image-48.png)
+
+![alt text](image-49.png)
+
+![alt text](image-50.png)
+
+![alt text](image-51.png)
+
+![alt text](image-52.png)
--- dev-sandbox/app.js
+++ dev-sandbox/app.js
@@ -1,5 +1,20 @@
 const list = [];
-Simon = {
+const finance = [];
+
+excelData = {
+   totalAmmount: 100000,
+   totalUsers: 1000,
+   totalRevenue: 500000,
+   totalExpenses: 200000
+};
+excelData2 = {
+    totalAmmount: 200000,
+    totalUsers: 2000,
+    totalRevenue: 1000000,
+    totalExpenses: 400000
+};
+
+user1 = {
     name: 'Simon',
     age: 24,
     city: 'Wheatland',
@@ -7,7 +22,7 @@ Simon = {
     role: 'Admin',
     mobilephone: ''
 };
-Von = {
+user2 = {
     name: 'Levon',
     age: 29,
     city: 'Austintown',
@@ -15,9 +30,14 @@ Von = {
     role: 'User',
     mobilephone: '123-456-7890'
 };
-list.push(Simon, Von);
 
-function checkMFA(user) {
+finance.push(excelData, excelData2);
+
+
+list.push(user1, user2);
+
+
+function checkPermissions(user) {
     if(user.mobilephone === ''){
         return 1 ,'User does not have MFA set';
     }
@@ -25,10 +45,10 @@ function checkMFA(user) {
         return 0 ,'User has MFA set';
     }
 }
-
-list.push(user1, user2);
+function checkData(array) {
+       console.table(array);
+}
 
 setInterval(() => {
-checkPermissions(user1);
-checkPermissions(user2);
-},1000);
+  console.table(finance);
+}, 5000);
